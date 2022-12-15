efbc68da6290ea0cf0bd92c183aa141329cda05e
Aditya Raj Singh <adi4adityaraj@gmail.com>
2022-12-15 17:18:48 +0530

    Add migrations

---
+++ app/models/mysql/big/Image.js
@@ -0,0 +1,111 @@
+/**
+ * DISCLAIMER: This file is example of boilerplate code. Please check before using it for particular use-case.
+ */
+const rootPrefix = '../../../..',
+  ModelBase = require(rootPrefix + '/app/models/mysql/Base'),
+  databaseConstants = require(rootPrefix + '/lib/globalConstant/database');
+
+// Declare variables names.
+const dbName = databaseConstants.userDbName;
+
+/**
+ * Class for image model.
+ *
+ * @class ImageModel
+ */
+class ImageModel extends ModelBase {
+  /**
+   * Constructor for user model.
+   *
+   * @augments ModelBase
+   *
+   * @constructor
+   */
+  constructor() {
+    super({ dbName: dbName });
+
+    const oThis = this;
+
+    oThis.tableName = 'images';
+  }
+
+  /**
+   * Format db data.
+   *
+   * @param {object} dbRow
+   * @param {string} dbRow.id
+   * @param {string} dbRow.url
+   * @param {string} dbRow.ipfs_object_id
+   * @param {number} dbRow.created_at
+   * @param {number} dbRow.updated_at
+   *
+   * @returns {object}
+   */
+  formatDbData(dbRow) {
+    const oThis = this;
+
+    const formattedData = {
+      id: dbRow.id,
+      url: dbRow.url,
+      ipfsObjectId: dbRow.ipfs_object_id,
+      createdAt: dbRow.created_at,
+      updatedAt: dbRow.updated_at
+    };
+
+    return oThis.sanitizeFormattedData(formattedData);
+  }
+
+  /**
+   * This method gets the response for the id passed.
+   *
+   * @param {string} id
+   *
+   * @returns {Promise<any>}
+   */
+  async getById(id) {
+    const oThis = this;
+
+    const response = await oThis
+      .select(['id', 'url', 'ipfs_object_id', 'created_at', 'updated_at'])
+      .where({ id: id })
+      .fire();
+
+    return response;
+  }
+
+  /**
+   * This method gets the response for the array of ids passed.
+   *
+   * @param {array} ids
+   *
+   * @returns {Promise<void>}
+   */
+  async getByIds(ids) {
+    const oThis = this;
+
+    const response = {};
+
+    const dbRows = await oThis
+      .select('*')
+      .where({ id: ids })
+      .fire();
+
+    for (let index = 0; index < dbRows.length; index++) {
+      const formatDbRow = oThis.formatDbData(dbRows[index]);
+      response[formatDbRow.id] = formatDbRow;
+    }
+
+    return response;
+  }
+
+  /**
+   * List of formatted column names that can be exposed by service.
+   *
+   * @returns {array}
+   */
+  safeFormattedColumnNames() {
+    return ['id', 'url', 'ipfsObjectId', 'createdAt', 'updatedAt'];
+  }
+}
+
+module.exports = ImageModel;
---
+++ db/migration/1671101649222_createImagesTable.js
@@ -0,0 +1,29 @@
+const rootPrefix = '../..',
+  database = require(rootPrefix + '/lib/globalConstant/database'),
+  dbKindConstants = require(rootPrefix + '/lib/globalConstant/dbKind');
+
+const dbName = database.bigDbName;
+const dbKind = dbKindConstants.sqlDbKind;
+
+const upQueries = [
+  'CREATE TABLE `images` (\n' +
+    '  `id` bigint(20) NOT NULL AUTO_INCREMENT,\n' +
+    '  `url` varchar(255) COLLATE utf8_unicode_ci ,\n' +
+    '  `ipfs_object_id` varchar(255) COLLATE DEFAULT NULL,\n' +
+    '  `created_at` int(11) NOT NULL,\n' +
+    '  `updated_at` int(11) NOT NULL,\n' +
+    '  PRIMARY KEY (`id`)\n' +
+    ') ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;'
+];
+
+const downQueries = ['DROP TABLE if exists `images`;'];
+
+const CreateImagesTable = {
+  dbName: dbName,
+  up: upQueries,
+  down: downQueries,
+  dbKind: dbKind,
+  tables: ['images']
+};
+
+module.exports = CreateImagesTable;
