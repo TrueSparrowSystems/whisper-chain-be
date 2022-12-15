2d117f522259e83aca75e8167c0cad0e41bda2ef
VRDighe <vaibhav@plgworks.com>
2022-12-15 17:11:46 +0530

    minor

--- db/migration/1671098359146_createWhispersTable.js
+++ db/migration/1671098359146_createWhispersTable.js
@@ -13,7 +13,7 @@ const upQueries = [
     '  `image_id` bigint(20) NOT NULL,\n' +
     '  `platform` tinyint(4) NOT NULL,\n' +
     '  `platform_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,\n' +
-    '  `platform_url` varchar(255) COLLATE utf8_unicode_ci DEFAULT node,\n' +
+    '  `platform_url` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,\n' +
     '  `ipfs_object_id` bigint(20) NOT NULL,\n' +
     '  `status` tinyint(4) NOT NULL,\n' +
     '  `created_at` int(11) NOT NULL,\n' +
@@ -22,7 +22,7 @@ const upQueries = [
     '  UNIQUE KEY `uk_1` (image_id), \n' +
     '  KEY `index_1` (chain_id, created_at), \n' +
     '  KEY `index_2` (platform_id, status, platform_url) \n' +
-    ') ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;'
+    ') ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;'
 ];
 
 const downQueries = ['DROP TABLE if exists `whispers`;'];
