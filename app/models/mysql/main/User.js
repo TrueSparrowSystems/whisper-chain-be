d9d93e195ba666fe9e74e60ef8367592af183892
VRDighe <vaibhav@plgworks.com>
2022-12-22 12:46:38 +0530

    update user model and add constants file

--- app/models/mysql/main/User.js
+++ app/models/mysql/main/User.js
@@ -1,6 +1,10 @@
 const rootPrefix = '../../../..',
   ModelBase = require(rootPrefix + '/app/models/mysql/Base'),
-  databaseConstants = require(rootPrefix + '/lib/globalConstant/database');
+  databaseConstants = require(rootPrefix + '/lib/globalConstant/database'),
+  localCipher = require(rootPrefix + '/lib/localCipher'),
+  util = require(rootPrefix + '/lib/util'),
+  cookieConstants = require(rootPrefix + '/lib/globalConstant/cookie'),
+  coreConstants = require(rootPrefix + '/config/coreConstants');
 
 // Declare variables names.
 const dbName = databaseConstants.mainDbName;
@@ -144,6 +148,109 @@ class UserModel extends ModelBase {
     return response;
   }
 
+  /**
+   * Fetch secure user by ids.
+   *
+   * @param {array} ids
+   *
+   * @returns {array}
+   */
+  async fetchByPlatformUserId(platformUserId) {
+    const oThis = this;
+
+    const dbRows = await oThis
+      .select('*')
+      .where({
+        platform_user_id: platformUserId
+      })
+      .fire();
+
+    const response = [];
+
+    for (let index = 0; index < dbRows.length; index++) {
+      const formatDbRow = oThis.formatDbData(dbRows[index]);
+
+      response.push(formatDbRow);
+    }
+
+    return response;
+  }
+
+  /**
+   * Get cookie value .
+   *
+   * @param {object} userObj
+   * @param {string} decryptedEncryptionSalt
+   * @param {object} options
+   * @param {number} options.timestamp
+   * @param {string} options.platform
+   *
+   * @returns {string}
+   */
+  getCookieValue(userObj, decryptedEncryptionSalt, options) {
+    const oThis = this;
+
+    const cookieToken = oThis.getCookieToken(userObj, decryptedEncryptionSalt, options);
+
+    if (!userObj.id) {
+      throw new Error(`Invalid userId-${options.userId} for getCookieValue`);
+    }
+
+    return (
+      cookieConstants.latestVersion +
+      ':' +
+      userObj.id +
+      ':' +
+      options.platform +
+      ':' +
+      options.timestamp +
+      ':' +
+      cookieToken
+    );
+  }
+
+  /**
+   * Get cookie token.
+   *
+   * @param {object} userObj
+   * @param {string} decryptedEncryptionSalt
+   * @param {object} options
+   * @param {number} options.timestamp
+   * @param {string} options.platform
+   *
+   * @returns {string}
+   */
+  getCookieToken(userObj, decryptedEncryptionSalt, options) {
+    console.log(userObj, decryptedEncryptionSalt, options);
+    const decryptedCookieToken = localCipher.decrypt(decryptedEncryptionSalt, userObj.cookieToken);
+
+    let strSecret = null;
+
+    strSecret = coreConstants.API_COOKIE_SECRET;
+
+    const stringToSign =
+      userObj.id +
+      ':' +
+      options.platform +
+      ':' +
+      options.timestamp +
+      ':' +
+      strSecret +
+      ':' +
+      decryptedCookieToken.substring(0, 16);
+    const salt =
+      userObj.id +
+      ':' +
+      options.platform +
+      ':' +
+      decryptedCookieToken.slice(-16) +
+      ':' +
+      strSecret +
+      ':' +
+      options.timestamp;
+
+    return util.createSha256Digest(salt, stringToSign);
+  }
   /**
    * List of formatted column names that can be exposed by service.
    *
