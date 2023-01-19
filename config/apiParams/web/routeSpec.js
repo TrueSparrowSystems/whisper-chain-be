09d6e2e2a927c527928e7290ea3d1569866e0f7a
kedarchandrayan <kedarchandrayan@gmail.com>
2023-01-19 15:50:02 +0530

    route spec changes.

--- config/apiParams/web/routeSpec.js
+++ config/apiParams/web/routeSpec.js
@@ -4,51 +4,52 @@ const rootPrefix = '../../..',
 const webRouteSpec = {
   'POST /api/lens/connect': {
     apiName: apiNameConstants.lensConnect,
-    summary: '',
-    tag: '',
-    description: ''
+    summary: 'User connect',
+    tag: 'Connect',
+    description: 'User is logged in and cookie created for handling auth in subsequent requests.'
   },
 
   'GET /api/lens/chains': {
     apiName: apiNameConstants.fetchChains,
-    summary: '',
-    tag: '',
-    description: ''
+    summary: 'Get chains to be shown on homepage.',
+    tag: 'Home Page',
+    description: 'Returns a list of chains and at max 3 whispers from each chain. Pagination support is present.'
   },
 
   'GET /api/lens/:chain_id': {
     apiName: apiNameConstants.whispers,
-    summary: '',
-    tag: '',
-    description: ''
+    summary: 'Get whispers from a specific chain',
+    tag: 'Chain Page',
+    description: 'Returns list of whispers which belong to a specific chain. Pagination support is present.'
   },
 
   'POST /api/lens/whispers': {
     apiName: apiNameConstants.createWhisper,
-    summary: '',
-    tag: '',
-    description: ''
+    summary: 'Add to chain',
+    tag: 'Generate Page',
+    description: 'Add to chain and create a whisper.'
   },
 
   'POST /api/lens/ipfs-objects': {
     apiName: apiNameConstants.ipfsObjects,
-    summary: '',
-    tag: '',
-    description: ''
+    summary: 'Upload to IPFS',
+    tag: 'Generate Page',
+    description: 'Upload image and metadata to IPFS and return cid in response.'
   },
 
   'POST /api/lens/logout': {
     apiName: apiNameConstants.logout,
-    summary: '',
-    tag: '',
+    summary: 'Logout',
+    tag: 'Logout',
     description: ''
   },
 
   'GET /api/images': {
     apiName: apiNameConstants.suggestions,
-    summary: '',
-    tag: '',
-    description: ''
+    summary: 'Generate suggestions',
+    tag: 'Generate Page',
+    description:
+      'Using stable diffusion in the BE, generate image suggestions using the prompt and art style input from user.'
   }
 };
 
