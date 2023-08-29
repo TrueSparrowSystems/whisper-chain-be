541b000e9ca477058b0eb91400eed15326a61442
Shraddha Falane <falane.shraddha18@gmail.com>
2023-08-29 14:02:03 +0530

    Removed unused files

--- cron/whisperStatusPollCron.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-i=0;
-while [ $i -lt 6 ]; do # 6 ten-second intervals in 1 minute
-  /usr/local/bin/node /usr/src/app/lib/cron/whisperStatusPolling.js >> /tmp/whisperStatusPolling.log
-  sleep 10
-  i=$(( i + 1 ))
-done
